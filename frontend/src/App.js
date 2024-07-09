97e2ed23a19c46bd8e18c36719aea7a8e03f6a4d
Kamiltczarnik <kamiltczarnik@gmail.com>
2024-07-09 15:38:48 -0500

    changes made to hover nav items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,15 +47,3 @@ function App() {
 }
 
 export default App;
-//have to link where we want the data to go to here.
-<form className="new-info-form">
-  <div className="form-row">
-    <label htmlfor="fname">First Name </label>
-    <input type="text" id="fname" name="fname"></input>
-    <br></br>
-    <label htmlfor="lname">Last Name </label>
-    <input type="text" id="lname" name="lname"></input>
-    <br></br>
-    <input type="submit" value="Submit"></input>
-  </div>
-</form>;
