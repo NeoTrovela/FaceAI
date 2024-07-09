ce954e3894d1bc95b6c4d8db37659d690041b72f
NeoTrovelaVillamiel <ntrovelavillamiel@gmail.com>
2024-07-09 11:22:44 -0500

    adding to views.py in django_project

--- backend/django_project/myapp/views.py
+++ backend/django_project/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def api_data(request):
+    data = {
+        "key": "value",
+        "message": "Django"
+    }
+
+    return JsonResponse(data)
