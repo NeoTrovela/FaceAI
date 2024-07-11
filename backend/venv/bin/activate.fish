20ce140900af46d6d10fdc4747e38978ca8bc4ea
NeoTrovelaVillamiel <ntrovelavillamiel@gmail.com>
2024-07-11 00:26:18 -0500

    Fixing venv and making right downloads

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/neo/Desktop/AIApp/backend/venv"
+set -gx VIRTUAL_ENV "/Users/neo/Documents/GitHub/FaceAI/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
