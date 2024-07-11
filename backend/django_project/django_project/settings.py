96acc2ab6ee80bb084da3612d2cd34033393529a
NeoTrovelaVillamiel <ntrovelavillamiel@gmail.com>
2024-07-10 22:46:41 -0500

    Adding apps in settings.py, used from downloaded libraries

--- backend/django_project/django_project/settings.py
+++ backend/django_project/django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myapp",
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "django_project.urls"
